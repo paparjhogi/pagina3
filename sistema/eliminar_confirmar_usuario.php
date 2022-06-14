bd49bd1ad592e127cdf701ffb7bd69cce956ccdf
jhogicito <jhogipapar2021@gmail.com>
2022-06-14 08:04:21 -0500

    subida

---
+++ sistema/eliminar_confirmar_usuario.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<?php include "includes/scripts.php"; ?>
+	<title>Eliminar usuario</title>
+</head>
+<body>
+	<?php include "includes/header.php"; ?>
+	<section id="container">
+		<h1>Eliminar usuario</h1>
+
+		
+	</section>
+
+	<?php include "includes/footer.php"; ?>
+</body>
+</html>
